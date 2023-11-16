7bed47659c7577c63ee291307be904759345790b
emanfj <syedaeman999@gmail.com>
2023-11-16 18:15:42 +0500

    footer css added

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Footer.css';
 import { FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa'; 
 import { BiMap } from 'react-icons/bi';
+import Logo from '../../assets/Fav_Icon.png';
 
 const Footer = () => {
   return (
     <footer className="footer-container">
         <div className="left-section">
-            <img src="" alt="logo"/>
+            <img src={Logo} alt="logo"/>
             <div className="contact-info">
                 <p>Phone: 000-0000000</p>
                 <p>Email: contact@ecobricks.com</p>
@@ -26,14 +27,16 @@ const Footer = () => {
         <div className="right-section">
             <div className="newletter">
                 <p>Subscribe for Updates</p>
-                <input type="email" placeholder="Your email here"/> 
-                <button>Subscibe</button>
+                <div className="input-wrapper">
+                    <input type="email" placeholder="Your email here"/> 
+                    <button>Subscibe</button>
+                </div>
             </div>
         </div>
         <hr className="divider"/>
         <div className="bottom-section">
             <div className="footer-links">
-                <p><a href="privacy-policy">privacy policy</a> | <a href="/sitemap">sitemap</a></p>
+                <p><a href="privacy-policy">privacy policy</a> |  <a href="/sitemap">sitemap</a></p>
             </div>
             <div className="copyright">
                 <p>&copy; {new Date().getFullYear()} ecobricks</p>
