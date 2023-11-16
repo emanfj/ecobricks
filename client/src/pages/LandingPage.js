14930b8950ae92d8a1ba4d2afe24777942f7a5e2
mahnoor <m.zamirrsh@gmail.com>
2023-11-16 17:07:25 +0500

    hero added

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from '../components/Hero/Hero'; 
 import Footer from '../components/Footer/Footer'; 
 
 const LandingPage = () => {
   return (
     <div>
+      <Hero /> 
       <Footer /> 
     </div>
   );
