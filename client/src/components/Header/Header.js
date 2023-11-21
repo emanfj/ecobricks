149249ba1be1325e92d5c7a15e7e435f675a15d5
mahnoor <m.zamirrsh@gmail.com>
2023-11-21 15:42:31 +0500

    header and story section updated

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,55 +1,53 @@
+import React, { useState } from 'react';
 import './Header.css';
-const React = require('react');
-const { useState } = require('react');
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-    };
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div class="header">
-            <div class="header-box">
-                <div class="logo-small-1">
-                    <img src={require('./Fav_Icon.png')} alt="" />
-                </div>
-                <div class="ecobricks-website">Ecobricks Website</div>
-                <div>
-                {/* Menu Icon */}
-                <div class={`hamburger ${isOpen ? 'open' : ''}`} onClick={handleClick}>
-                    <div class="hamburger"></div>
-                    <div class="hamburger" style="top: 28px;"></div>
-                    <div class="hamburger" style="top: 37px;"></div>
-                </div>
-
-                {/* Menu Items */}
-                {isOpen && (
-                    <>
-                    <div class="crossbox">
-                        <div class="line_7"></div>
-                        <div class="line_8"></div>
-                    </div>
-                    <div class="about">
-                        <div class="about_text">About</div>
-                    </div>
-                    <div class="blog">
-                        <div class="blog_text">Blog</div>
-                    </div>
-                    <div class="contact">
-                        <div class="contact_text">Contact</div>
-                    </div>
-                    <div class="products">
-                        <div class="products_text">Products</div>
-                    </div>
-                    </>
-                )}
-            </div>
-            </div>
+  return (
+    <div className="header">
+      <div className="header-box">
+        <div className="logo-small-1">
+          <img src={require('./Fav_Icon.png')} alt="" />
         </div>
-    );
-};
+        <div className="ecobricks-website">Ecobricks Website</div>
+        <div>
+          {/* Menu Icon */}
+          <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={handleClick}>
+            <div className="hamburger"></div>
+            <div className="hamburger" style={{ top: '28px' }}></div>
+            <div className="hamburger" style={{ top: '37px' }}></div>
+          </div>
 
+          {/* Menu Items */}
+          {isOpen && (
+            <>
+              <div className="crossbox">
+                <div className="line_7"></div>
+                <div className="line_8"></div>
+              </div>
+              <div className="about">
+                <div className="about_text">About</div>
+              </div>
+              <div className="blog">
+                <div className="blog_text">Blog</div>
+              </div>
+              <div className="contact">
+                <div className="contact_text">Contact</div>
+              </div>
+              <div className="products">
+                <div className="products_text">Products</div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- client/src/components/Story/Story.js
+++ client/src/components/Story/Story.js
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Story.css';
 
 const Story = () => {
-    const [state, setState] = useState({
-      name: '',
-      isEditing: false,
-    });
-    
-    return (
-        <div class="story">
-        <div class="title_textbox">
-            <div class="dot1"></div>
-            <div class="ourstory">Our Story</div>
-            <div class="dot2"></div>
-        </div>
-        <div class="story_of_company">blas blas blas</div>
-        <div class="ellipse"></div>
-        <div class="ellipse4"></div>
-        <div class="ellipse3"></div>
+  const [state, setState] = useState({
+    name: '',
+    isEditing: false,
+  });
+
+  return (
+    <div className="story">
+      <div className="title_textbox">
+        <div className="dot1"></div>
+        <div className="ourstory">Our Story</div>
+        <div className="dot2"></div>
+      </div>
+      <div className="story_of_company">blas blas blas</div>
+      <div className="ellipse"></div>
+      <div className="ellipse4"></div>
+      <div className="ellipse3"></div>
     </div>
-    )
-}
+  );
+};
 
 export default Story;
