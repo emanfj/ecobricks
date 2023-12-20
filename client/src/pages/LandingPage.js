9bb691599efabc06ea7564f54222e63032eaeb73
emanfj <syedaeman999@gmail.com>
2023-12-21 04:06:48 +0500

    page view updated

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -9,6 +9,8 @@ import Header from '../components/Header/Header';
 import Story from '../components/Story/Story';
 import CompanyProjects from '../components/CompanyProjects/CompanyProjects';
 import FeatureImage from '../assets/feature-image.png';
+import BrickColorsInsight from '../components/BrickColorsInsight/BrickColorsInsight'
+import SDG from '../components/SDG/SDG'
 
 const LandingPage = () => {
   const headerStyle = {
@@ -27,12 +29,16 @@ const LandingPage = () => {
       <Story /> 
       <Heading title="PRODUCTS" detail="Lorem Gpsum Dolor Sit Amet" />
       <Products />
+      <Heading title="GREEN INTERLOCKING PAVING TILE" detail="Lorem Gpsum Dolor Sit Amet" />
+      <BrickColorsInsight />
       <Heading title="PROJECTS" detail="Lorem Gpsum Dolor Sit Amet" />
       <CompanyProjects />
-      <Heading title="SOCIAL PROOF" detail="Lorem Gpsum Dolor Sit Amet" />
-      <SocialProof />
       <Heading title="IMPACT" detail="Lorem Gpsum Dolor Sit Amet" />
       <Impact />
+      <Heading title="SUSTAINABLE DEVELOPMENT GOALS" detail="Lorem Gpsum Dolor Sit Amet" />
+      <SDG />
+      <Heading title="SOCIAL PROOF" detail="Lorem Gpsum Dolor Sit Amet" />
+      <SocialProof />
       <Footer /> 
     </div>
   );
